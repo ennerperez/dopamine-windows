ff8f3e490a0505b2ae48b6b5f919008a2c65a6c5
Enner Pérez <ennerperez@gmail.com>
2023-04-04 01:31:49 -0500

    Amphetamine Data

---
+++ Amphetamine.Data/PlaybackCounters.cs
@@ -0,0 +1,16 @@
+﻿
+namespace Dopamine.Data
+{
+    public class PlaybackCounter
+    {
+        public string Path { get; set; }
+
+        public long PlayCount { get; set; }
+
+        public long SkipCount { get; set; }
+
+        public long DateLastPlayed { get; set; }
+
+        public string SafePath { get; set; }
+    }
+}
